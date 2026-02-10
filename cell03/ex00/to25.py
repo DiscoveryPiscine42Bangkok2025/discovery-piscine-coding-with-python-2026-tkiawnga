8222f164dd392e33b6d1f1444d06fc58c95d8c3c
tkiawnga <66050168@kmitl.ac.th>
2026-02-10 11:50:59 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,6 @@
+num = int(input('Enter the number less than 25: '))
+while(num<25):
+    num+=1
+    print(f"Inside the loop, my variable is {num}")
+if num>25:
+    print("Error")
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+say = input('What you gotta say?: ')
+while True :
+    got = input('I got that! Anything else? :')
+    if got=='stop...':
+        print('I got that! Anything else? : STOP')
+        break
