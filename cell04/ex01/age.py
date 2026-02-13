eb74bcd2a1742df25e69e9b05cff64ad1f690d2f
tkiawnga <66050168@kmitl.ac.th>
2026-02-13 15:58:41 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,5 @@
+your_age = int(input("Please tell me your age: "))
+print("You are currently " + str(your_age) + " years old.")
+print("in 10 years,you'll be "+str(your_age+10)+" year old.")
+print("in 20 years,you'll be "+str(your_age+20)+" year old.")
+print("in 30 years,you'll be "+str(your_age+30)+" year old.")
---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def divide(x,y):
+    return x/y
+def multi(x,y):
+    return x*y
+first_number = int(input("Give me the first number = "))
+second_number = int(input("Give me the second number = "))
+print(str(first_number)+" + "+str(second_number)+" = "+str(add(first_number,second_number)))
+print(str(first_number)+" - "+str(second_number)+" = "+str(sub(first_number,second_number)))
+print(str(first_number)+" x "+str(second_number)+" = "+str(multi(first_number,second_number)))
+if second_number !=0:
+    print(str(first_number)+" / "+str(second_number)+" = "+str(divide(first_number,second_number)))
+else:
+    print("undefined (division by zero)") 
